3d4f84e2948d1ffd3e441a5a56d9cd06bd18dcc4
LucidLunacy <rmathers@live.co.uk>
2021-12-08 18:21:08 +0000

    Size efficient images, add connect to metamask buttons in useful places

---
+++ src/components/AddNetworkComponent/AddNetworkComponent.js
@@ -0,0 +1,107 @@
+const Ethers = require('ethers');
+
+import React, { Component } from 'react';
+import './add.css';
+
+class AddNetworkComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      address: null,
+      added: false,
+      network: this.props.network,
+      window: null
+    }
+
+    this.connectWallet = this.connectWallet.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({address: window.ethereum.selectedAddress});
+  }
+
+  async connectWallet() {
+
+    const mainnet = {
+      chainId: '0x' + (57).toString(16),
+      chainName: 'Syscoin Mainnet',
+      iconUrls: ['https://syscoin.org/images/syscoin-blue-flat-logo.svg'],
+      nativeCurrency: {
+        name: 'Syscoin',
+        symbol: 'SYS',
+        decimals: 18,
+      },
+      rpcUrls: ['https://rpc.syscoin.org/', 'wss://rpc.syscoin.org/wss'],
+      blockExplorerUrls: ['https://explorer.syscoin.org']
+    }
+
+    const testnet = {
+      chainId: '0x' + (5700).toString(16),
+      chainName: 'Syscoin Tanenbaum Testnet',
+      iconUrls: ['https://syscoin.org/images/syscoin-blue-flat-logo.svg'],
+      nativeCurrency: {
+        name: 'Test Syscoin',
+        symbol: 'tSYS',
+        decimals: 18,
+      },
+      rpcUrls: ['https://rpc.tanenbaum.io/'],
+      blockExplorerUrls: ['https://tanenbaum.io/']
+    }
+
+    if (window.ethereum) {
+      // request connecting to wallet
+      await window.ethereum.send('eth_requestAccounts');
+
+      var req;
+      switch (this.state.network) {
+        case 'Mainnet':
+          req = {
+            method: 'wallet_addEthereumChain',
+            params: [mainnet, this.state.selectedAddress]
+          }
+          break;
+        case 'Tanenbaum Testnet':
+          req = {
+            method: 'wallet_addEthereumChain',
+            params: [testnet, this.state.selectedAddress]
+          }
+        default:
+          break;
+      }
+
+      try {
+        // request to add chain
+        await window.ethereum.request(req)
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+      this.setState({added: true});
+      return true;
+    }
+  }
+
+  render() {
+
+    if (!this.state.added) {
+      return (
+          <section className="section">
+            <button onClick={this.connectWallet} className="addButton">
+              Add to MetaMask
+            </button>
+          </section>
+      );
+    } else {
+      return (
+          <section className="section">
+            <button className="addButton">
+              Added
+            </button>
+          </section>
+      );
+    }
+  }
+}
+
+export default AddNetworkComponent;
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -5,43 +5,57 @@ import styles from './HomepageFeatures.module.css';
 const FeatureList = [
   {
     title: 'Developer Documentation',
-    png: 'img/getSys-02.png',
+    png: 'img/getSys-01.png',
+    pngSmall: 'img/getSys-01-sm.png',
     link: 'docs/dev-resources/nevm/resources',
     color: '#DCEAFF',
   },
   {
     title: 'Deploy a NEVM Smart Contract',
-    png: 'img/feature-function-01.png',
+    png: 'img/feature-function-02.png',
+    pngSmall: 'img/feature-function-02-sm.png',
     link: 'docs/dev-resources/nevm/guides-and-tuts',
     color: '#DDEBF6',
   },
   {
     title: 'What is Syscoin?',
-    png: 'img/feature-security-01.png',
+    png: 'img/feature-security-02.png',
+    pngSmall: 'img/feature-security-02-sm.png',
     link: 'docs/intro/syscoin-what',
     color: '#CFE8EB',
   },
   {
     title: 'User Guides',
-    png: 'img/connect-02.png',
+    png: 'img/connect-01.png',
+    pngSmall: 'img/connect-01-sm.png',
     link: 'docs/guides/overview',
     color: '#EBE8CF',
   },
   {
     title: 'Get SYS',
-    png: 'img/exchanges-02.png',
+    png: 'img/exchanges-01.png',
+    pngSmall: 'img/exchanges-01-sm.png',
     link: 'docs/dev-resources/tsys',
     color: '#EBD2CF',
   },
 ];
 
-function Feature({png, title, description, link, color}) {
+function Feature({png, pngSmall, title, description, link, color}) {
 
   return (
     <div className={clsx(styles.box)}>
       <a href={link} className={styles.featureButton}>
         <div className={clsx('col col--2'), styles.feature} style={{background: color}}>
-          <img className={styles.featurePng} alt={title} src={png} />
+        <picture>
+           <source
+              media="(min-width: 650px)"
+              srcSet={png} />
+           <source
+              media="(min-width: 200px)"
+              srcSet={pngSmall} />
+            <img className={styles.featurePng} alt={title} src={png} />
+        </picture>
+
           <h3>{title}</h3>
         </div>
       </a>
